cbbdd867546528dbd63e2d734f921a25c85dafb2
Ankit <av5120634@gmail.com>
2024-02-26 17:49:59 +0530

    Add ReplaceAllSubstrings function

--- SubStringsIndex.cpp
+++ SubStringsIndex.cpp
@@ -1,4 +1,4 @@
-//find index of all substring matches.
+//find index of all substring matches. and replace them
 #include<iostream>
 #include<vector>
 
@@ -21,23 +21,48 @@ std::vector<int> FindSubstringIndexes(const std::string& phrase, const std::stri
     return matches;
 }
 
+std::vector<int> ReplaceAllSubstrings( std::string& phrase, std::string& subString, std::string& newString) {
+    std::vector<int> matches =  FindSubstringIndexes(phrase,subString);
+    
+    if(matches.size() != 0) {
+        int lengthDifference = newString.size() - subString.size();
+        int timesLooped = 0;
+        for(auto index: matches) {
+        std::cout << subString.size() << " lengthdifference: " << lengthDifference << "Times Looped: " << timesLooped << std::endl;
+            phrase.replace(index + (timesLooped * lengthDifference),//finds the next index for replacement 
+            subString.size(), newString);
+
+            timesLooped++;
+        }
+    }
+    return matches;
+}
+
 int main() {
     std::string phrase;
     std::string subString;
+    std::string newString;
     std::cout << "Enter the phrase: ";
     std::cin >> phrase;
     std::cout << "Enter the substring: ";
     std::cin >> subString;
+    std::cout << "Enter the newstring: ";
+    std::cin >> newString;
 
     std::vector<int> matches = FindSubstringIndexes(phrase,subString);
 
+    ReplaceAllSubstrings(phrase,subString,newString);
+
     if(matches.empty())
         std::cout << "No matches found: " << std::endl;
-    else 
-        std::cout << "Matched Indexes: ";
+    else {
+     std::cout << "Matched Indexes: ";
         for(int x: matches) {
             std::cout << x << " ";
-        }
-    
+        }   
+    }
+
+    std::cout << phrase << std::endl;
+
     return 0;
 }
